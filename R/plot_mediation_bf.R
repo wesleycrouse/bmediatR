e0d39ee4ab0fe0c86ad5756539025504a36c7f45
Greg Keele <keeleg@mlg-bh0089.lan>
2020-08-17 17:10:19 -0400

    Added genome-wide posterior probability plot

--- R/plot_mediation_bf.R
+++ R/plot_mediation_bf.R
@@ -114,3 +114,70 @@ plot_bf <- function(med_bf_object,
         col = "red")
   }
 }
+
+#' @export
+#' @examples plot_posterior()
+plot_posterior <- function(med_bf_object, 
+                           post_col = c(4, 8),
+                           med_annot, 
+                           include_chr = c(1:19, "X"), 
+                           expand_lim_factor = 0.025, 
+                           label_thresh = NULL, 
+                           bgcol = "white", altcol = "gray", altbgcol = "white", hlines_col = "gray80", col = "black", cex = 0.75,
+                           qtl_dat = NULL,
+                           outcome_symbol = NULL,
+                           ...) {
+  
+  post_p <- matrix(apply(exp(med_bf_object$ln_post_c[,c(4,8)]), 1, function(x) sum(x)), ncol = 1)
+  rownames(post_p) <- rownames(med_bf_object$ln_post_c)
+  class(post_p) <- "scan1"
+  
+  med_map_df <- med_annot %>%
+    dplyr::select(protein.id, symbol, chr, middle) %>%
+    dplyr::filter(chr %in% include_chr) %>%
+    dplyr::mutate(chr = factor(chr, levels = c(1:19, "X"))) %>%
+    as.data.frame %>% 
+    dplyr::arrange(chr)
+  if (!is.null(qtl_dat)) {
+    ## Add QTL to map for plotting
+    med_map_df <- dplyr::bind_rows(med_map_df,
+                                   data.frame(protein.id = "QTL", symbol = "QTL", chr = qtl_dat$chr, middle = qtl_dat$pos))
+    
+  }
+  med_map <- map_df_to_list(map = med_map_df, marker_column = "protein.id", pos_column = "middle")
+  
+  gap <- sum(qtl2::chr_lengths(map))/100
+  
+  lim_shift <- (max(post_p[,1]) - min(post_p[,1])) * expand_lim_factor
+  qtl2:::plot.scan1(post_p, map = med_map, ylab = "Posterior Probability", type = "p", pch = 20, ylim = c(min(post_p[,1]) - lim_shift, max(post_p) + lim_shift),
+                    bgcol = bgcol, altcol = altcol, altbgcol = altbgcol, hlines_col = hlines_col, col = col, cex = cex, gap = gap,
+                    ...)
+  
+  xpos <- qtl2:::map_to_xpos(map = med_map, gap = gap)
+  
+  if (!is.null(label_thresh) & any(post_p > label_thresh)) {
+    labels <- rownames(post_p)[post_p > label_thresh]
+    
+    label_map_df <- med_map_df %>%
+      filter(protein.id %in% labels) 
+    
+    for (i in 1:nrow(label_map_df)) {
+      lab_pos <- xpos[label_map_df$protein.id[i]]
+      lab_post_p <- post_p[label_map_df$protein.id[i],]
+      
+      text(x = lab_pos, y = lab_post_p, label_map_df$symbol[i], font = 3)
+    }
+  }
+  if (!is.null(outcome_symbol)) {
+    rug(x = xpos[med_annot %>% 
+                   dplyr::filter(symbol == outcome_symbol) %>% pull(protein.id)],
+        lwd = 3,
+        col = "black")
+  }
+  if (!is.null(qtl_dat)) {
+    rug(x = xpos["QTL"],
+        lwd = 3,
+        col = "red")
+  }
+}
+
