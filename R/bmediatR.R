2bf1f47bad404fcd9c44904a55f015b9d4700dfe
Greg Keele <keeleg@mlg-bh0089.lan>
2020-10-12 13:34:59 -0400

    minor adjustment to avoid meaningless warning

--- R/bmediatR.R
+++ R/bmediatR.R
@@ -79,11 +79,11 @@ posterior_summary <- function(ln_prob_data, ln_prior_c, c_numerator, c_denominat
   }
   
   #presets for ln_prior_c; 
-  if (ln_prior_c=="complete"){
+  if (ln_prior_c[1]=="complete"){
     ln_prior_c <- c(rep(0,8), rep(-Inf,4))
-  } else if (ln_prior_c=="partial"){
+  } else if (ln_prior_c[1]=="partial"){
     ln_prior_c <- c(rep(-Inf,4), rep(0,4), rep(-Inf,4))
-  } else if (ln_prior_c=="reactive"){
+  } else if (ln_prior_c[1]=="reactive"){
     ln_prior_c <- rep(0,12)
   }
   
